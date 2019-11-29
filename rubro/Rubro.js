594426e62e916e53d5195cc3d7f6503264ab7f6f
Jraviolo <38055263+Jraviolo@users.noreply.github.com>
2019-11-28 21:57:13 -0300

    Create Rubro.js

---
+++ rubro/Rubro.js
@@ -0,0 +1,48 @@
+import React from 'react';|
+import {View, Text, Button, TextInput, Picker, Switch} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+const estilos = StyleSheet.create({
+    contenedor: {
+    flex: 1,
+    flexDirection:'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor:'#3eb3b1'
+    width:'100%',
+    },
+     titulo: {
+     color: '#FF0000',
+     backgroundColor: '#F3F3F3',
+     fontWeight: 'bold',
+     fontSize: 30,
+     width: '100%',
+     },
+});
+
+const Rubro = () => {
+
+const [flag,setFlag] = useState(false);
+
+return(
+
+    <View>
+      <Text>Rubro</Text>
+      <Text>Nombre</Text>
+      <TextInput />
+      <Text>Orden en el catalogo</Text>
+      <Picker>
+          <Picker.Item Label="#1" value="1"/>
+          <Picker.Item Label="#2" value="2"/>
+          </Picker>
+       <Text>Destacar</Text>
+      <Switch
+          value={flag}
+          onValueChange={ () => setFlag(!flag)}
+          />
+      <Button title="Guardar" />
+    </View>
+  );
+};
+
+export default Rubro;
